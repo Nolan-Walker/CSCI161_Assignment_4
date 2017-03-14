06d727f3fed1a77df768f7c1c15ca0cd1cb450ed
Nolan Caissie <nolancaissie@node-1w7jr9sr3a9ketvsyp7j3ckf4.ipv6.telus.net>
2017-03-14 16:00:38 -0700

    Initial Commit

---
+++ Assignment4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment4
+//
+//  Created by Nolan Caissie on 2017-03-14.
+//  Copyright © 2017 Nolan Caissie. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
